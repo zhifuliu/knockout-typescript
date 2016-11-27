4d7d55b5cc234b52381721c2d7f58322f79381a3
zhifu <zhifu@Ocsens-MacBook-Pro.local>
2016-11-27 13:28:42 +0800

    添加 hello.ts，有一个 class 定义，并使用了 knockout

---
+++ src/hello.ts
@@ -0,0 +1,12 @@
+import * as ko from "knockout";
+
+class HelloViewModel {
+    language: KnockoutObservable<string>
+    framework: KnockoutObservable<string>
+
+    constructor(language: string, framework: string) {
+        this.language = ko.observable(language);
+        this.framework = ko.observable(framework);
+    }
+}
+ko.applyBindings(new HelloViewModel('typescript', 'Knockout'));
